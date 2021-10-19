12595d26e6004c629b7d93bd37e3787260389baf
Konstantin Aksenov <aksenov.kostya@gmail.com>
2021-10-18 23:58:38 -0700

    Fix codestyle

--- compose/src/main/kotlin/io/github/kakaocup/compose/node/matchers/CustomMatchers.kt
+++
@@ -1,8 +0,0 @@
-package io.github.kakaocup.compose.node.matchers
-
-import androidx.compose.ui.test.SemanticsMatcher
-
-/**
- * Matcher that returns any node in the hierarchy.
- */
-internal fun isAny() = SemanticsMatcher("isAny") { true }
