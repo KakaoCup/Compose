bd1618493961772b7db1664c21233b3a82dd6a69
Konstantin Aksenov <aksenov.kostya@gmail.com>
2023-11-17 09:12:00 +1000

    try-fix

--- compose/build.gradle.kts
+++ compose/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.library")
     kotlin ("android")
-    id("org.jetbrains.dokka")
+    //id("org.jetbrains.dokka")
 }
 
 android {
@@ -23,18 +23,41 @@ android {
             java.srcDir("src/test/kotlin")
         }
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlin {
+        jvmToolchain(17)
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.isFork = true
+
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
 }
 
 dependencies {
     implementation(libs.androidx.test.espresso.espressoCore)
-    implementation(libs.androidx.test.ext.junit)
+    //implementation(libs.androidx.test.ext.junit)
 
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui.uiTestJunit4)
+    //implementation(platform(libs.androidx.compose.bom))
+    implementation("androidx.compose.ui:ui-test-junit4-android:1.5.4")
 
-    dokkaHtmlPlugin(libs.org.jetbrains.dokka.kotlinAsJavaPlugin)
+    //dokkaHtmlPlugin(libs.org.jetbrains.dokka.kotlinAsJavaPlugin)
 }
 
+/*
 tasks.dokkaGfm {
     moduleName.set("compose")
     outputDirectory.set(File("$rootDir/docs"))
@@ -44,6 +67,8 @@ tasks.dokkaHtml.configure {
     outputDirectory.set(File("$rootDir/html"))
 }
 
+ */
+
 afterEvaluate {
-    Deployment.initialize(project)
+    //Deployment.initialize(project)
 }
