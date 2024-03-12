f89497a59d3b6421633073de86b28f0d1744b139
roschin-vy <vladimir_roschin@rambler.ru>
2024-03-12 13:17:55 +0300

    up gradle to 8.6 and up other libs

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -9,8 +9,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
-    implementation("com.android.tools.build:gradle:8.2.1")
-    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
-    implementation("org.kohsuke:github-api:1.318")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    implementation("com.android.tools.build:gradle:8.3.0")
+    implementation("org.jetbrains.dokka:dokka-gradle-plugin:1.9.20")
+    implementation("org.kohsuke:github-api:1.319")
 }
