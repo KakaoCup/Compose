b690441e11530130dd9b1ad2e26f8544e10343f6
Konstantin Aksenov <aksenov.kostya@gmail.com>
2024-10-12 22:02:05 +0200

    docs: fix docusaurus build

--- documentation/docusaurus.config.ts
+++ documentation/docusaurus.config.ts
@@ -88,7 +88,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/introduction',
             },
           ],
         },
